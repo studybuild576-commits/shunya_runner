261ba61f0e2ad04cc63024ed98b9895976f9e3d1
studybuild576-commits <studybuild576@gmail.com>
2025-09-17 17:35:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+// YEH IMPORT LINE SABSE ZAROORI HAI
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:flame/input.dart'; // Required for keyboard, tap, drag
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -16,94 +16,69 @@ void main() {
   );
 }
 
-class ShunyaRunnerGame extends Forge2DGame {
+class ShunyaRunnerGame extends Forge2DGame
+    with KeyboardEvents, PointerMoveCallbacks, TapCallbacks {
   late PlayerBody player;
   Vector2 mousePosition = Vector2.zero();
-
-  ShunyaRunnerGame() : super(gravity: Vector2.zero());
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-
-    // Camera setup
     camera.viewfinder.zoom = 1.5;
     camera.viewfinder.anchor = Anchor.center;
 
-    // Floor sprite
     final sprite = await loadSprite('floor_tile.png');
     add(
-      SpriteComponent(
+      SpriteRepeatComponent(
         sprite: sprite,
         size: Vector2.all(400),
-        anchor: Anchor.center,
-      ),
+      )..anchor = Anchor.center
     );
 
-    // Arena
     add(Arena(size: Vector2.all(200)));
 
-    // Player
     player = PlayerBody(position: Vector2.zero());
     add(player);
 
-    // Enemies
     add(EnemyBody(position: Vector2(100, 100), player: player));
     add(EnemyBody(position: Vector2(-100, -100), player: player));
-
-    // Input handlers
-    add(KeyboardHandler(player: player));
-    add(MouseHandler(player: player));
   }
 
   @override
   void update(double dt) {
     super.update(dt);
+    player.lookAt(mousePosition);
   }
-}
-
-// Separate component for keyboard input
-class KeyboardHandler extends Component with KeyboardHandlerComponent {
-  final PlayerBody player;
-  KeyboardHandler({required this.player});
 
   @override
-  KeyEventResult onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+  void onPointerMove(PointerMoveEvent event) {
+    mousePosition = screenToWorld(event.localPosition);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    super.onTapDown(event);
+    final tapPosition = screenToWorld(event.localPosition);
+    final direction = (tapPosition - player.body.position)..normalize();
+    final velocity = direction * 500.0;
+    final bullet = BulletBody(
+      position: player.body.position.clone(),
+      initialVelocity: velocity,
+    );
+    add(bullet);
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+    KeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
     Vector2 newMovement = Vector2.zero();
     if (keysPressed.contains(LogicalKeyboardKey.keyW)) newMovement.y = -1;
     if (keysPressed.contains(LogicalKeyboardKey.keyS)) newMovement.y = 1;
     if (keysPressed.contains(LogicalKeyboardKey.keyA)) newMovement.x = -1;
     if (keysPressed.contains(LogicalKeyboardKey.keyD)) newMovement.x = 1;
     player.movement = newMovement;
-    return KeyEventResult.handled;
-  }
-}
-
-// Separate component for mouse/tap input
-class MouseHandler extends Component with TapCallbacks, DragCallbacks {
-  final PlayerBody player;
-  Vector2 mousePosition = Vector2.zero();
-
-  MouseHandler({required this.player});
-
-  @override
-  bool onTapDown(TapDownEvent event) {
-    final tapPosition = event.localPosition.toVector2();
-    final direction = (tapPosition - player.body.position)..normalize();
-    final velocity = direction * 500.0;
-
-    final bullet = BulletBody(
-      position: player.body.position.clone(),
-      initialVelocity: velocity,
-    );
-    player.parent?.add(bullet);
-    return true;
-  }
-
-  @override
-  bool onDragUpdate(int pointerId, DragUpdateEvent event) {
-    mousePosition = event.localPosition.toVector2();
-    player.lookAt(mousePosition);
-    return true;
+    return super.onKeyEvent(event, keysPressed);
   }
 }
