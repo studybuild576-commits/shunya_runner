330601f968599f934e15bc77a47baaa43aa0a106
studybuild576-commits <studybuild576@gmail.com>
2025-09-17 17:02:56 +0530

    Create arena.dart

---
+++ lib/components/arena.dart
@@ -0,0 +1,22 @@
+// lib/components/arena.dart
+import 'package:flame_forge2d/flame_forge2d.dart';
+
+class Arena extends BodyComponent {
+  final Vector2 size;
+  Arena({required this.size});
+
+  @override
+  Body createBody() {
+    final bodyDef = BodyDef(type: BodyType.static, position: Vector2.zero());
+    final arenaBody = world.createBody(bodyDef);
+    final vertices = [
+      size,
+      Vector2(size.x, -size.y),
+      -size,
+      Vector2(-size.x, size.y),
+    ];
+    final chain = ChainShape()..createLoop(vertices);
+    arenaBody.createFixture(FixtureDef(chain));
+    return arenaBody;
+  }
+}
