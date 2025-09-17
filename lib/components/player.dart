0b41e719dbe57a4ef2ed6d2c3723f148f824ffae
studybuild576-commits <studybuild576@gmail.com>
2025-09-17 19:57:32 +0530

    Update player.dart

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -3,13 +3,13 @@ import 'package:flame/components.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 
 class PlayerBody extends BodyComponent {
-  @override
-  final Vector2 position;
   final double radius = 8.0;
   Vector2 movement = Vector2.zero();
   final double speed = 200.0;
-  
-  PlayerBody({required this.position});
+
+  final Vector2 initialPosition;
+
+  PlayerBody({required this.initialPosition});
 
   @override
   Future<void> onLoad() async {
@@ -26,10 +26,17 @@ class PlayerBody extends BodyComponent {
 
   @override
   Body createBody() {
-    final bodyDef = BodyDef(type: BodyType.dynamic, position: position, fixedRotation: true, linearDamping: 0.5, userData: this);
+    final bodyDef = BodyDef(
+        type: BodyType.dynamic,
+        position: initialPosition,
+        fixedRotation: true,
+        linearDamping: 0.5,
+        userData: this);
     final playerBody = world.createBody(bodyDef);
     final shape = CircleShape(radius: radius);
-    final fixtureDef = FixtureDef(shape)..density = 1.0..friction = 0.4;
+    final fixtureDef = FixtureDef(shape)
+      ..density = 1.0
+      ..friction = 0.4;
     playerBody.createFixture(fixtureDef);
     return playerBody;
   }
@@ -37,11 +44,16 @@ class PlayerBody extends BodyComponent {
   @override
   void update(double dt) {
     super.update(dt);
-    body.linearVelocity = movement * speed;
+    if (isMounted) {
+      body.linearVelocity = movement * speed;
+    }
   }
-  
+
   void lookAt(Vector2 target) {
-    final angle = atan2(target.y - body.position.y, target.x - body.position.x);
-    body.setTransform(body.position, angle);
+    if (isMounted) {
+      final angle =
+          atan2(target.y - body.position.y, target.x - body.position.x);
+      body.setTransform(body.position, angle);
+    }
   }
 }
