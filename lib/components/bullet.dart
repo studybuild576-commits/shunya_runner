d481c4dd114db1c23ad44be9dd3581fce3658fe5
studybuild576-commits <studybuild576@gmail.com>
2025-09-17 11:18:17 +0530

    Update bullet.dart

--- lib/components/bullet.dart
+++ lib/components/bullet.dart
@@ -1,8 +1,8 @@
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
+import 'package:shunya_runner/components/enemy.dart';
 
-class BulletBody extends BodyComponent {
-  // YAHAN BADLAAV KIYA GAYA HAI: @override add kiya
+class BulletBody extends BodyComponent with ContactCallbacks {
   @override
   final Vector2 position;
   final double radius = 1.5;
@@ -14,15 +14,22 @@ class BulletBody extends BodyComponent {
     final bodyDef = BodyDef(
       type: BodyType.dynamic,
       position: position,
-      bullet: true, // Tej-raftaar collision ke liye
+      bullet: true,
       userData: this,
     );
     final bulletBody = world.createBody(bodyDef);
     final shape = CircleShape()..radius = radius;
-    final fixtureDef = FixtureDef(shape)
-      ..isSensor = true; // Bullet takrane par bounce na ho, sirf takkar detect ho
+    final fixtureDef = FixtureDef(shape)..isSensor = true;
     bulletBody.createFixture(fixtureDef);
     return bulletBody;
+  }
+
+  @override
+  void beginContact(Object other, Contact contact) {
+    if (other is EnemyBody) {
+      other.removeFromParent();
+      removeFromParent();
+    }
   }
 
   @override
